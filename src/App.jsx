c197496f34ed9a234244536999a25d0eeabde7a0
jirwincline <jakobinwins@gmail.com>
2019-10-11 19:42:02 -0700

    create marioplan app

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { BrowserRouter, Link } from 'react-router-dom';
+
+import Routes from './Routes';
+
+
+const App = () => (
+  <BrowserRouter>
+    <main className="container">
+      <h1>Marioplan</h1>
+    </main>
+  </BrowserRouter>
+);
+
+export default App;
---
+++ src/components/layouts/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Navbar = () => {
+    return (
+        <nav className="nav-wrapper grey darken-3">
+            <div className="container">
+                
+            </div>
+        </nav>
+    )
+}
