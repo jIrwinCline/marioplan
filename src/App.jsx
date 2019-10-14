825f564e6ae3eb87102bd83a23d6da6bfe1785aa
jirwincline <jakobinwins@gmail.com>
2019-10-13 19:46:43 -0700

    make signin component

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,25 @@
-import React from 'react';
-import { BrowserRouter, Link } from 'react-router-dom';
+import React, { Component } from 'react';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 import Navbar from './components/layouts/Navbar';
 import Routes from './Routes';
-
-
-const App = () => (
-  <BrowserRouter>
-    <div className="App">
-      <Navbar />
-    </div>
-  </BrowserRouter>
-);
+import Dashboard from './components/dashboard/Dashboard'
+import ProjectDetails from './components/projects/ProjectDetails'
+import SignIn from './components/auth/SignIn'
+class App extends Component {
+  render() {
+    return(
+      <BrowserRouter>
+        <div className="App">
+          <Navbar />
+          <Switch>
+            <Route exact path='/' component={Dashboard} />
+            <Route path='/project/:id' component={ProjectDetails} />
+            <Route path='/signin' component={SignIn} />
+          </Switch>
+        </div>
+      </BrowserRouter>
+    )
+  }
+}
 
 export default App;
--- src/components/dashboard/Dashboard.jsx
+++ src/components/dashboard/Dashboard.jsx
@@ -1,14 +1,21 @@
 import React, { Component } from 'react';
-
+import Notifications from './Notifications';
+import ProjectList from '../projects/ProjectList'
 class Dashboard extends Component {
     render(){
         return (
             <div className="dashboard container">
                 <div className="row">
-                    <div className="col s12 m6"></div>
-                    <div className="col s12 m5 offset-m1"></div>
+                    <div className="col s12 m6">
+                        <ProjectList />
+                    </div>
+                    <div className="col s12 m5 offset-m1">
+                        <Notifications />
+                    </div>
                 </div>
             </div>
         )
     }
 }
+
+export default Dashboard
