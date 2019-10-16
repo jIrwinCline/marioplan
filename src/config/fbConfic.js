787a03ccb2fe1a68dcdf3ffa3d8cb7f6c1901239
jirwincline <jakobinwins@gmail.com>
2019-10-15 20:18:13 -0700

    add fbConfig file to work with firebase

---
+++ src/config/fbConfic.js
@@ -0,0 +1,20 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore'
+import 'firebase/auth'
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyB3ULjCPnl1oyyVwk_bIRqu9M1OqBSOrCA",
+    authDomain: "marioplan-821a8.firebaseapp.com",
+    databaseURL: "https://marioplan-821a8.firebaseio.com",
+    projectId: "marioplan-821a8",
+    storageBucket: "marioplan-821a8.appspot.com",
+    messagingSenderId: "1421824979",
+    appId: "1:1421824979:web:f0d73f72abf226b3d3d526",
+    measurementId: "G-LFHN8Y2DS6"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.firestore().settings({ timestampsInSnapshots: true })
+
+export default firebase;
