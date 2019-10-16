508a4ab54ed3572c60e59b2396a37ddb33f121f1
jirwincline <jakobinwins@gmail.com>
2019-10-15 20:05:43 -0700

    fix import issue on CreateProject, and complete entered project to console log correctly

--- src/components/projects/CreateProject.jsx
+++ src/components/projects/CreateProject.jsx
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import createProject from '../../store/actions/projectActions';
+import { createProject } from '../../store/actions/projectActions';
 
 class CreateProject extends Component {
     state = {
@@ -14,7 +14,8 @@ class CreateProject extends Component {
     }
     handleSubmit = (e) => {
         e.preventDefault();
-        console.log(this.state);
+        // console.log(this.state);
+        this.props.createProject(this.state)
     }
     render() {
         return (
